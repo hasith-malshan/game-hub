e2dbb7640504e6087093eac265cb00882c952947
hmr53 <h.malshan.53@gmail.com>
2024-03-02 11:48:50 +0530

    Refactor : Extract query object.

--- src/components/Platformselector.tsx
+++ src/components/Platformselector.tsx
@@ -2,7 +2,6 @@ import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 import { BsChevronDown } from 'react-icons/bs';
 import usePlatforms from '../hooks/usePlatform';
 import { Platform } from '../hooks/useGames';
-
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
   selectedPlatform: Platform | null;
